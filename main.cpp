339080c8f7c12444d1fc1f94e40d236dc2cf4b20
Keano Ricketts <keano.ricketts@yahoo.com>
2024-12-18 17:15:00 +0000

    Added some code which makes use of SFML to render a blank game window.

--- main.cpp
+++ main.cpp
@@ -2,4 +2,18 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(1080,720), "Hungry Snake");
+
+    while (window.isOpen()){
+        sf::Event event{};
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
 }
+
