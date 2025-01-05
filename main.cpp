a3d2523dfe87af9ed25de98074c937e7310013d0
Keano Ricketts <keano.ricketts@yahoo.com>
2025-01-05 12:00:03 +0000

    Improvements to the game window.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Headers/Snake.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800,600), "Hungry Snake");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Hungry Snake");
+    window.setFramerateLimit(60); // Limits the framerate
 
-    while (window.isOpen()){
+    // The game loop
+    while (window.isOpen()) {
         sf::Event event{};
-        while (window.pollEvent(event)){
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
+        // Clear the window with a color
+        window.clear(sf::Color(201, 253, 201)); // Green background
 
+        // Display the content
         window.display();
     }
-}
 
+    return 0;
+}
