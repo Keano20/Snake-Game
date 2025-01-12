8332b95c05efaae5b60366c29afca4a9dfd12a16
Keano Ricketts <keano.ricketts@yahoo.com>
2025-01-12 14:33:48 +0000

    Changes to the food class.

--- Headers/Food.h
+++ Headers/Food.h
@@ -2,9 +2,29 @@
 #define SNAKE_GAME_FOOD_H
 
 
+#include <cstdlib> // For rand()
+#include <ctime>   // For seeding random number generator
+
 class Food {
+private:
+    int x;
+    int y;
+    int gridWidth;
+    int gridHeight;
 
+public:
+    Food(int gridWidth, int gridHeight) : gridWidth(gridWidth), gridHeight(gridHeight) {
+        std::srand(std::time(0));
+        generateNewPosition();
+    }
+
+    void generateNewPosition() {
+        x = std::rand() % gridWidth;
+        y = std::rand() % gridHeight;
+    }
+
+    int getX() const { return x; }
+    int getY() const { return y; }
 };
-
 
 #endif //SNAKE_GAME_FOOD_H
