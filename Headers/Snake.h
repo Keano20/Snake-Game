de4fb90803afe344b485f8492b9fb2c42ea893fc
Keano Ricketts <keano.ricketts@yahoo.com>
2025-01-13 18:55:47 +0000

    Created a basic Snake class.

--- Headers/Snake.h
+++ Headers/Snake.h
@@ -1,10 +1,24 @@
 #ifndef SNAKE_GAME_SNAKE_H
 #define SNAKE_GAME_SNAKE_H
 
+#include <SFML/Graphics.hpp>
+#include <vector>
 
 class Snake {
+private:
+    std::vector<sf::RectangleShape> body;
+    sf:: Vector2f direction;
+    float blockSize;
+    bool canChangeDirection;
 
+public:
+    Snake(float blockSize);
+    void move();
+    void grow();
+    void setDirection(sf::Vector2f newDirection);
+    sf::Vector2f getHeadPosition() const;
+    const std::vector<sf::RectangleShape>& getBody() const;
+    bool checkSelfCollision() const;
 };
-
 
 #endif //SNAKE_GAME_SNAKE_H
