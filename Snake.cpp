967d7752d2e141d8091156d79dd67445cbb3e807
Keano Ricketts <keano.ricketts@yahoo.com>
2025-01-15 10:54:28 +0000

    Added snake constructor to initialise the body and start position.

--- Snake.cpp
+++ Snake.cpp
@@ -1,1 +1,11 @@
 #include "Headers/Snake.h"
+
+// Constructor: Initialize the snake
+Snake::Snake(float blockSize)
+        : blockSize(blockSize), direction({1, 0}), canChangeDirection(true) {
+    sf::RectangleShape head(sf::Vector2f(blockSize, blockSize));
+    head.setFillColor(sf::Color::Green);
+    head.setPosition(100.f, 100.f); // Starting position
+    body.push_back(head);
+}
+
